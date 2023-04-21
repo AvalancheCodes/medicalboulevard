7447c5c373c03f5ce1c7fc3d34ec965ea57156de
Sergey Sosunov <ser.sosunov@live.com>
2023-04-21 19:48:43 +0300

    Footer UI updates

--- components/partials/RealEstatePageLayout.tsx
+++ components/partials/RealEstatePageLayout.tsx
@@ -14,34 +14,58 @@ import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import ImageLoader from "../ImageLoader"
 import StickyNavbar from "../StickyNavbar"
 import StarRating from "../StarRating"
-import SocialButton from "../SocialButton"
-import MarketButton from "../MarketButton"
 import SignInModalLight from "../partials/SignInModalLight"
 import SignUpModalLight from "../partials/SignUpModalLight"
 import ScheduleTourButton from "../ScheduleTourButton";
 import Cart from "../Cart";
 import useAuthContext from "../../state/AuthStateProvider/useAuthContext";
 
-// Footer recent blog posts array
-const footerPosts = [
-  {
-    href: '/real-estate/blog-single',
-    img: '/images/real-estate/blog/th01.jpg',
-    category: 'Home Improvement',
-    title: 'Your Guide to a Smart Apartment Searching',
-    text: 'Mi justo, varius vitae cursus ipsum sem massa amet pellentesque. Ipsum enim sit nulla ridiculus semper nam...',
-    date: 'Dec 4',
-    comments: '2'
-  },
-  {
-    href: '/real-estate/blog-single',
-    img: '/images/real-estate/blog/th02.jpg',
-    category: 'Tips & Advice',
-    title: 'Top 10 Ways to Refresh Your Space',
-    text: 'Volutpat, orci, vitae arcu feugiat vestibulum ultricies nisi, aenean eget. Vitae enim, tellus tempor consequat mi vitae...',
-    date: 'Nov 23',
-    comments: 'No'
-  }
+// Links
+const footerLinks = [
+  [{
+    title: "Links", items: [
+      { title: "Home", href: "/" },
+      { title: "Medical Rooms", href: "/rooms" },
+      { title: "Services", href: "/services" },
+      { title: "Blog", href: "/" },
+      { title: "Directory", href: "/" },
+    ]
+  }],
+  [{
+    title: "Company", items: [
+      { title: "Company", href: "/" },
+      {
+        title: "Careers",
+        href: "/",
+        decorator: "<span class='badge rounded-pill bg-warning text-dark'>We're hiring</span>"
+      },
+      { title: "Contacts", href: "/" },
+      { title: "Security", href: "/" },
+    ]
+  }],
+  [{
+    title: "Featured Services", items: [
+      { title: "Web & Mobile Dev", href: "/" },
+      { title: "Local Digital Marketing", href: "/" },
+      { title: "China Marketing ", href: "/" },
+      { title: "MSO Services", href: "/" },
+      { title: "Digital Content Creation", href: "/" },
+    ]
+  }],
+  [
+    {
+      title: "Legal", items: [
+        { title: "Terms of Use", href: "/" },
+        { title: "Privacy Policy", href: "/" },
+      ]
+    },
+    {
+      title: "Docs", items: [
+        { title: "Documentation", href: "/" },
+        { title: "Snippets", href: "/" },
+      ]
+    }
+  ]
 ]
 
 interface IProps {
@@ -203,11 +227,7 @@ const RealEstatePageLayout = ({ pageTitle, activeNav, navbarExtraClass, children
                       className='order-lg-3'>
                 <i className='fi-cart'></i>
                 <Badge pill bg="warning" className='position-absolute'
-                       style={{
-                         fontSize: '0.4rem',
-                         right: '5px',
-                         top: '7px'
-                       }}
+                       style={{ fontSize: '0.4rem', right: '5px', top: '7px' }}
                 > 1 </Badge>
               </Button>
             </OverlayTrigger>
@@ -310,160 +330,75 @@ const RealEstatePageLayout = ({ pageTitle, activeNav, navbarExtraClass, children
 
 
       {/* Footer */}
-      <footer className='footer bg-secondary pt-5'>
+      <footer className="footer bg-primary pt-5 text-secondary">
         <Container className='pt-lg-4 pb-4'>
           <Row className='mb-5 pb-md-3 pb-lg-4'>
-            <Col lg={6} className='mb-lg-0 mb-4'>
+            <Col xs={12} className='mb-lg-0 mb-4'>
               <div className='d-flex flex-sm-row flex-column justify-content-between mx-n2'>
 
                 {/* Logo + contacts */}
                 <div className='mb-sm-0 mb-4 px-2'>
-                  <Link href='/real-estate' className='d-inline-flex mb-4'>
+                  <Link href="/" className='d-inline-flex mb-2'>
                     <ImageLoader
                       priority
-                      src='/images/logo/logo-dark.png'
-                      width={116} height={32}
+                      src='/images/logo/mdb-footer-logo.png'
+                      width={244} height={118}
                       placeholder={"blur"}
                       alt='MedicalBoulevard'
                     />
                   </Link>
-                  <Nav className='flex-column mb-sm-4 mb-2'>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link href='mailto:example@email.com' active={false} className='p-0 fw-normal'>
-                        <i className='fi-mail me-2 align-middle opacity-70'></i>
-                        example@email.com
-                      </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link href='tel:4065550120' active={false} className='p-0 fw-normal'>
-                        <i className='fi-device-mobile me-2 align-middle opacity-70'></i>
-                        (406) 555-0120
-                      </Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <div className='pt-2'>
-                    <SocialButton href='#' variant='solid' brand='facebook' roundedCircle className='me-2 mb-2'/>
-                    <SocialButton href='#' variant='solid' brand='twitter' roundedCircle className='me-2 mb-2'/>
-                    <SocialButton href='#' variant='solid' brand='viber' roundedCircle className='me-2 mb-2'/>
-                    <SocialButton href='#' variant='solid' brand='telegram' roundedCircle className='mb-2'/>
+                  <div className="pt-2 d-flex flex-row justify-content-center">
+                    <Button as={Link as any} href="#" className="btn-social-icon">
+                      <i className="fa-brands fa-facebook"></i>
+                    </Button>
+                    <Button as={Link as any} href="#" className="btn-social-icon">
+                      <i className="fa-brands fa-twitter"></i>
+                    </Button>
+                    <Button as={Link as any} href="#" className="btn-social-icon">
+                      <i className="fa-brands fa-github"></i>
+                    </Button>
+                    <Button as={Link as any} href="#" className="btn-social-icon">
+                      <i className="fa-brands fa-slack"></i>
+                    </Button>
                   </div>
                 </div>
 
                 {/* Quick links */}
-                <div className='mb-sm-0 mb-4 px-2'>
-                  <h4 className='h5'>Quick Links</h4>
-                  <Nav className='flex-column'>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Buy a property</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Sell a property</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Rent a property</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Calculate your
-                        property</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Top offers</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Top cities</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                </div>
-
-                {/* About links */}
-                <div className='px-2'>
-                  <h4 className='h5'>About</h4>
-                  <Nav className='flex-column'>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>About us</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Our agents</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false}
-                                className='p-0 fw-normal'>Help &amp; support</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>Contacts</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className='mb-2'>
-                      <Nav.Link as={Link} href='#' active={false} className='p-0 fw-normal'>News</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                </div>
+                {footerLinks.map((section, index) => (
+                  <div key={index}>
+                    {section.map((block, i) => (
+                      <div className={`mb-sm-0 px-2 ${i > 0 && "mt-4"}`} key={block.title}>
+                        <h4 className="fs-xs text-secondary fw-normal mb-4" style={{
+                          textDecorationLine: "underline",
+                          textTransform: "uppercase"
+                        }}>
+                          {block.title}
+                        </h4>
+                        <Nav className="flex-column">
+                          {block.items.map((l) => (
+                            <Nav.Item className="my-1" key={l.title}>
+                              <Nav.Link as={Link} href={l.href} active={activeNav === l.href}
+                                        className="p-0 fs-sm text-secondary fw-normal">
+                                {l.title}
+                                {l.decorator && (
+                                  <div className="d-inline ms-1"
+                                       dangerouslySetInnerHTML={{ __html: l.decorator }}></div>)}
+                              </Nav.Link>
+                            </Nav.Item>
+                          ))}
+                        </Nav>
+                      </div>
+                    ))}
+                  </div>
+                ))}
               </div>
-            </Col>
-
-            {/* Recent posts */}
-            <Col lg={6} xl={{ span: 5, offset: 1 }}>
-              <h4 className='h5'>Recent Posts</h4>
-              {footerPosts.map((post, indx) => (
-                <div key={indx}>
-                  <article className='d-flex align-items-start' style={{ maxWidth: '640px' }}>
-                    <Link href={post.href} className='d-none d-sm-flex flex-shrink-0 mb-sm-0 mb-3'
-                          style={{ width: '100px', height: '100px' }}>
-                      <ImageLoader src={post.img} width={200} height={200} className='rounded-3' alt='Thumbnail'/>
-                    </Link>
-                    <div className='ps-sm-4'>
-                      <h6 className='mb-1 fs-xs fw-normal text-uppercase text-primary'>{post.category}</h6>
-                      <h5 className='mb-2 fs-base'>
-                        <Link href={post.href} className='nav-link'>{post.title}</Link>
-                      </h5>
-                      <p className='mb-2 fs-sm'>{post.text}</p>
-                      <Link href='#' className='nav-link nav-link-muted d-inline-block me-3 p-0 fs-xs fw-normal'>
-                        <i className='fi-calendar mt-n1 me-1 fs-sm align-middle opacity-70'></i>
-                        {post.date}
-                        \ </Link>
-                      <Link href='#' className='nav-link nav-link-muted d-inline-block p-0 fs-xs fw-normal'>
-                        <i className='fi-chat-circle mt-n1 me-1 fs-sm align-middle opacity-70'></i>
-                        {`${post.comments} comments`}
-                      </Link>
-                    </div>
-                  </article>
-                  {indx < footerPosts.length - 1 && <hr className='text-dark opacity-10 my-4'/>}
-                </div>
-              ))}
             </Col>
           </Row>
 
-          {/* Mobile app CTA */}
-          <div className='bg-dark rounded-3'>
-            <Col xs={10} md={11} xxl={10}
-                 className='d-flex flex-md-row flex-column-reverse align-items-md-end align-items-center mx-auto px-0'>
-              <div className='d-flex flex-shrink-0 mt-md-n5 me-md-5'>
-                <ImageLoader
-                  priority
-                  src='/images/real-estate/illustrations/mobile.svg'
-                  width={240}
-                  height={237}
-                  alt='Illustration'/>
-              </div>
-              <div
-                className='align-self-center d-flex flex-lg-row flex-column align-items-lg-center pt-md-3 pt-5 ps-xxl-4 text-md-start text-center'>
-                <div className='me-md-5'>
-                  <h4 className='text-light'>Download Our App</h4>
-                  <p className='mb-lg-0 text-light'>Find everything you need for buying, selling &amp; renting property
-                    in our new Finder App!</p>
-                </div>
-                <div className='flex-shrink-0'>
-                  <MarketButton href='#' market='apple' className='mx-2 ms-sm-0 me-sm-4 mb-3'/>
-                  <MarketButton href='#' market='google' className='mb-3'/>
-                </div>
-              </div>
-            </Col>
-          </div>
-
           {/* Copyright */}
-          <div className='text-center fs-sm pt-4 mt-3 pb-2'>
-            &copy; All rights reserved. Made by <a href='https://createx.studio/'
-                                                   className='d-inline-block nav-link p-0' target='_blank'
-                                                   rel='noreferrer'>Createx Studio</a>
+          <div className="d-flex flex-row justify-content-between">
+            <p>© Medical Boulevard Inc. 2023.</p>
+            <p>All rights reserved.</p>
           </div>
         </Container>
       </footer>
