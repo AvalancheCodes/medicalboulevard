0a427f3512f0ab15449960d9c7b286ffc8e3be5a
charmerdev1025 <charmerweb1025@gmail.com>
2023-05-23 08:36:42 -0500

    initial

---
+++ pages/space-landing/index.js
@@ -0,0 +1,9 @@
+export default function Space() {
+  return <main className="page-wrapper">
+    <div className="space-landing">
+      <section className="container-fluid header text-center">
+        <span className="display-3 text-primary fw-light">Flexible Medical Rooms Rentals</span>
+      </section>
+    </div>
+  </main>;
+}
