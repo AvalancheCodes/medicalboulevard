99e95b3e4a2dd243ca4f9356f7b3a82b64deefbc
charmerdev1025 <charmerweb1025@gmail.com>
2023-05-23 15:58:03 -0500

    space landing header done

--- pages/space-landing/index.js
+++ pages/space-landing/index.js
@@ -1,8 +1,22 @@
+import styles from './space.module.css'
 export default function Space() {
   return <main className="page-wrapper">
     <div className="space-landing">
-      <section className="container-fluid header text-center">
-        <span className="display-3 text-primary fw-light">Flexible Medical Rooms Rentals</span>
+      <section className={`${styles.header} text-white text-center`}>
+        <div className={styles.overlay}></div>
+        <div className={styles.content}>
+          <div className="py-5">
+            <img src='/images/space-landing/space_landing_header_logo.png'/>
+          </div>
+          <p className="display-3 fw-light"><span>Flexible Medical Rooms Rentals</span></p>
+          <p className="fs-6 fw-normal">
+            Establish Your Beverly Hills Presence - Flexible Medical Room Rentals for New and Expanding Practices
+          </p>
+          <p className="fs-7 fw-normal">
+            <i>Medical Boulevard Inc.</i>
+          </p>
+          <a className="btn btn-primary">REQUEST A TOUR</a>
+        </div>
       </section>
     </div>
   </main>;
