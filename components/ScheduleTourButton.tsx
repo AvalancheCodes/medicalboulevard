7c93a74b7b285f2d4e76b1c82fd510c575036435
charmerdev1025 <78768845+charmerdev1025@users.noreply.github.com>
2023-06-09 04:12:46 -0400

    fixed previous calendly popup using calendly modal

--- components/ScheduleTourButton.tsx
+++ components/ScheduleTourButton.tsx
@@ -1,7 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Link from "next/link";
-import { PopupModal } from 'react-calendly';
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import CalendlyModal from "./CalendlyModal";
 
 interface IProps {
   className?: string;
@@ -9,45 +9,29 @@ interface IProps {
 }
 
 const ScheduleTourButton = ({ className }: IProps) => {
-  var pageSettings = {
-    backgroundColor: 'ffffff',
-    hideEventTypeDetails: false,
-    hideLandingPageDetails: false,
-    primaryColor: '00a2ff',
-    textColor: '4d5055'
-  }
-  var utm = {
-    utmCampaign: 'Spring Sale 2019',
-    utmContent: 'Shoe and Shirts',
-    utmMedium: 'Ad',
-    utmSource: 'Facebook',
-    utmTerm: 'Spring'
-  }
-  const [isOpen, setIsOpen] = useState(false);
 
-  const openCalendlyModal = () => {
-    setIsOpen(true);
-  }
+  const [requestTourShow, setRequestTourShow] = useState(false)
+  const handleRequestTourShow = () => setRequestTourShow(true)
+  const handleRequestTourClose = useCallback(() => setRequestTourShow(false), []);
   // Render markup
   // if (typeof document !== 'undefined') {
     return (
       <>
+      <CalendlyModal
+        centered
+        size='xl'
+        show={requestTourShow}
+        onHide={handleRequestTourClose}
+      />
       {/* <Button as={Link as any} href='/real-estate/add-property' size='sm' className={className} onClick={openCalendlyModal}>
         <i className='fi-plus me-2'></i>
         Schedule a Tour
       </Button> */}
-      <Button size='sm' className={className} onClick={openCalendlyModal}>
+      <Button size='sm' className={className} onClick={handleRequestTourShow}>
         <i className='fi-plus me-2'></i>
         Schedule a Tour
       </Button>
-      <PopupModal
-          url="https://calendly.com/charmerweb1025/30min"
-          // pageSettings={pageSettings}
-          // utm={utm}
-          onModalClose={() => setIsOpen(false)}
-          open={isOpen}
-          rootElement={document.getElementById("__next")}
-        />
+
       </>
     )
   // }
